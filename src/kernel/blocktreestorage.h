2c792833836693940ba38b7a2befe9d55facacb3
alexanderwiederin <alex@wiederin.xyz>
2025-08-04 13:57:15 +0200

    Add directory lcoking and read-only mode to BlockTreeStore
    Implement directory locking using `.blocktreelock` file to prevent
    conflicts between multiple BlockTreeStore instances. Add read-only mode
    that skips log application during initialization and validates logs
    instead of applying them.

--- src/kernel/blocktreestorage.h
+++ src/kernel/blocktreestorage.h
@@ -59,6 +59,9 @@ private:
     fs::path m_block_files_file_path;
     fs::path m_reindex_flag_file_path;
     fs::path m_prune_flag_file_path;
+    fs::path m_path;
+    bool m_read_only;
+    bool m_directory_locked;
 
     const CChainParams m_params;
 
@@ -73,9 +76,11 @@ private:
     void CheckMagicAndVersion() const;
 
     [[nodiscard]] bool ApplyLog() const EXCLUSIVE_LOCKS_REQUIRED(m_mutex);
+    [[nodiscard]] bool ValidateLog() const EXCLUSIVE_LOCKS_REQUIRED(m_mutex);
 
 public:
-    BlockTreeStore(const fs::path& file_path, const CChainParams& params, bool wipe_data = false);
+    BlockTreeStore(const fs::path& file_path, const CChainParams& params, bool wipe_data = false, bool read_only = false);
+    ~BlockTreeStore();
 
     void ReadReindexing(bool& reindexing) const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
     void WriteReindexing(bool reindexing) const EXCLUSIVE_LOCKS_REQUIRED(!m_mutex);
