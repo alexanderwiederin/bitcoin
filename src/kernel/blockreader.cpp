74720a52f66deee656ce5e7f5762b792c300923c
alexanderwiederin <alex@wiederin.xyz>
2025-09-15 15:43:03 +0200

    add Handler to blockreader.cpp

--- src/kernel/blockreader.cpp
+++ src/kernel/blockreader.cpp
@@ -4,6 +4,51 @@
 
 #include <kernel/bitcoinkernel.h>
 #include <kernel/blockreader.h>
+#include <memory>
+#include <utility>
+
+namespace {
+template <typename C, typename CPP>
+struct Handle {
+    static C* ref(CPP* cpp_type)
+    {
+        return reinterpret_cast<C*>(cpp_type);
+    }
+
+    static const C* ref(const CPP* cpp_type)
+    {
+        return reinterpret_cast<const C*>(cpp_type);
+    }
+
+    template <typename... Args>
+    static C* create(Args&&... args)
+    {
+        auto cpp_obj{std::make_unique<CPP>(std::forward<Args>(args)...)};
+        return reinterpret_cast<C*>(cpp_obj.release());
+    }
+
+    static C* copy(const C* ptr)
+    {
+        auto cpp_obj{std::make_unique<CPP>(get(ptr))};
+        return reinterpret_cast<C*>(cpp_obj.release());
+    }
+
+    static const CPP& get(const C* ptr)
+    {
+        return *reinterpret_cast<const CPP*>(ptr);
+    }
+
+    static CPP& get(C* ptr)
+    {
+        return *reinterpret_cast<CPP*>(ptr);
+    }
+
+    static void operator delete(void* ptr)
+    {
+        delete reinterpret_cast<CPP*>(ptr);
+    }
+};
+} // namespace
 
 extern "C" {
 } // extern "C"
