0d06944a66664815b8b279be69a72723e01a5903
alexanderwiederin <alex@wiederin.xyz>
2025-09-15 11:20:05 +0200

    implement blockreader and expose key methods on the c api

--- src/kernel/bitcoinkernel.h
+++ src/kernel/bitcoinkernel.h
@@ -1334,6 +1334,36 @@ BITCOINKERNEL_API void btck_block_hash_destroy(btck_BlockHash* block_hash);
 
 ///@}
 
+typedef struct btck_BlockReaderOptions btck_BlockReaderOptions;
+
+typedef struct btck_BlockReader btck_BlockReader;
+
+BITCOINKERNEL_API btck_BlockReaderOptions* BITCOINKERNEL_WARN_UNUSED_RESULT btck_blockreader_options_create(
+    const btck_Context* context,
+    const char* blocks_directory,
+    size_t blocks_directory_len,
+    const char* data_directory,
+    size_t data_directory_len) BITCOINKERNEL_ARG_NONNULL(1, 2);
+
+BITCOINKERNEL_API void btck_blockreader_options_destroy(btck_BlockReaderOptions* blockreader_options);
+
+BITCOINKERNEL_API btck_BlockReader* BITCOINKERNEL_WARN_UNUSED_RESULT btck_blockreader_create(
+    const btck_BlockReaderOptions* blockreader_options) BITCOINKERNEL_ARG_NONNULL(1);
+
+BITCOINKERNEL_API void btck_blockreader_destroy(btck_BlockReader* blockreader);
+
+BITCOINKERNEL_API const btck_Chain* BITCOINKERNEL_WARN_UNUSED_RESULT btck_blockreader_get_validated_chain(const btck_BlockReader* blockreader) BITCOINKERNEL_ARG_NONNULL(1);
+
+BITCOINKERNEL_API btck_Block* BITCOINKERNEL_WARN_UNUSED_RESULT btck_blockreader_read_block(
+    const btck_BlockReader* blockreader,
+    const btck_BlockTreeEntry* block_tree_entry) BITCOINKERNEL_ARG_NONNULL(1, 2);
+
+BITCOINKERNEL_API btck_BlockSpentOutputs* BITCOINKERNEL_WARN_UNUSED_RESULT btck_blockreader_block_spent_outputs_read(
+    const btck_BlockReader* blockreader,
+    const btck_BlockTreeEntry* block_tree_entry) BITCOINKERNEL_ARG_NONNULL(1, 2);
+
+
+
 #ifdef __cplusplus
 } // extern "C"
 #endif // __cplusplus
