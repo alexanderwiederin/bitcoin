3ce35946f8c55b5e6182add8a20003602e0e66d7
alexanderwiederin <alex@wiederin.xyz>
2025-09-16 14:07:32 +0200

    merge blockreader into bitcoinkernel files

--- src/kernel/bitcoinkernel.cpp
+++ src/kernel/bitcoinkernel.cpp
@@ -2,6 +2,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#include "kernel/blockreader_opts.h"
+#include "kernel/reader_impl.h"
 #define BITCOINKERNEL_BUILD
 
 #include <kernel/bitcoinkernel.h>
@@ -10,6 +12,7 @@
 #include <coins.h>
 #include <consensus/amount.h>
 #include <consensus/validation.h>
+#include <kernel/blockreader_opts.h>
 #include <kernel/caches.h>
 #include <kernel/chainparams.h>
 #include <kernel/checks.h>
@@ -1130,4 +1133,76 @@ int btck_chain_contains(const btck_Chain* chain, const btck_BlockTreeEntry* entr
 {
     LOCK(::cs_main);
     return btck_Chain::get(chain).Contains(&btck_BlockTreeEntry::get(entry)) ? 1 : 0;
+}
+
+struct btck_BlockReaderOptions : Handle<btck_BlockReaderOptions, kernel::BlockReaderOpts> {
+};
+struct btck_BlockReader : Handle<btck_BlockReader, blockreader::BlockReader> {
+};
+
+btck_BlockReaderOptions* btck_blockreader_options_create(
+    const btck_Context* context,
+    const btck_ChainParameters* chain_parameters,
+    const char* blocks_directory,
+    size_t blocks_directory_len,
+    const char* data_directory,
+    size_t data_directory_len)
+{
+    try {
+        const auto& chainparams = *btck_ChainParameters::get(chain_parameters);
+
+        fs::path abs_blocks_dir{fs::absolute(fs::PathFromString({blocks_directory, blocks_directory_len}))};
+        fs::create_directories(abs_blocks_dir);
+        fs::path abs_data_dir{fs::absolute(fs::PathFromString({data_directory, data_directory_len}))};
+        fs::create_directories(abs_data_dir);
+
+        return btck_BlockReaderOptions::create(chainparams, abs_blocks_dir, abs_data_dir);
+    } catch (const std::exception& e) {
+        LogError("Failed to create block reader options: %s", e.what());
+        return nullptr;
+    }
+}
+
+void btck_blockreader_options_destroy(btck_BlockReaderOptions* blockreader_options)
+{
+    delete blockreader_options;
+}
+
+btck_BlockReader* btck_blockreader_create(const btck_BlockReaderOptions* blockreader_options)
+{
+    try {
+        const auto& opts{btck_BlockReaderOptions::get(blockreader_options)};
+
+        return btck_BlockReader::create(opts.chainparams, opts.data_dir, opts.blocks_dir);
+    } catch (const std::exception& e) {
+        LogError("Failed to create block reader: %s", e.what());
+        return nullptr;
+    }
+}
+
+void btck_blockreader_destroy(btck_BlockReader* blockreader)
+{
+    delete blockreader;
+}
+
+const btck_Chain* btck_blockreader_get_validated_chain(const btck_BlockReader* blockreader)
+{
+    const auto& reader = btck_BlockReader::get(blockreader);
+    return btck_Chain::ref(&reader.GetValidatedChain());
+}
+
+btck_Block* btck_blockreader_read_block(
+    const btck_BlockReader* blockreader,
+    const btck_BlockTreeEntry* block_tree_entry)
+{
+    const auto& reader = btck_BlockReader::get(blockreader);
+    const auto& block_index = btck_BlockTreeEntry::get(block_tree_entry);
+
+    auto block = std::make_shared<CBlock>();
+    if (!reader.GetBlockManager().ReadBlock(*block, block_index)) {
+        LogError("BlockReader: Failed to read block");
+        return nullptr;
+    }
+
+    return btck_Block::create(block);
 }
