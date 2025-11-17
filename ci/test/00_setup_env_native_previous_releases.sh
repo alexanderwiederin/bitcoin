f77ac3483d9221abb3730086429f1ff733405a68
alexanderwiederin <alex@wiederin.xyz>
2025-11-17 15:49:39 +0100

    only run compat test

--- ci/test/00_setup_env_native_previous_releases.sh
+++ ci/test/00_setup_env_native_previous_releases.sh
@@ -6,22 +6,25 @@
 
 export LC_ALL=C.UTF-8
 
-export CONTAINER_NAME=ci_native_previous_releases
-export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:22.04"
-# Use minimum supported python3.10 and gcc-12, see doc/dependencies.md
-export PACKAGES="gcc-12 g++-12 python3-zmq"
-export DEP_OPTS="CC=gcc-12 CXX=g++-12"
-export TEST_RUNNER_EXTRA="--previous-releases --coverage --extended --exclude feature_dbcrash"  # Run extended tests so that coverage does not fail, but exclude the very slow dbcrash
+export CONTAINER_NAME=ci_native_previous_releases_minimal
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:24.04"
+export PACKAGES="gcc g++ python3-zmq libsqlite3-dev libevent-dev libboost-dev"
+export DEP_OPTS=""
+export RUN_UNIT_TESTS=false
+export RUN_FUNCTIONAL_TESTS=true
+export TEST_RUNNER_EXTRA="--previous-releases feature_coinstatsindex_compatibility.py"
 export GOAL="install"
 export CI_LIMIT_STACK_SIZE=1
 export DOWNLOAD_PREVIOUS_RELEASES="true"
+
+export NO_DEPENDS=1
+
 export BITCOIN_CONFIG="\
  --preset=dev-mode \
+ -DBUILD_GUI=OFF \
+ -DWITH_ZMQ=OFF \
+ -DWITH_USDT=OFF \
+ -DENABLE_IPC=OFF \
  -DREDUCE_EXPORTS=ON \
- -DCMAKE_BUILD_TYPE=Debug \
- -DCMAKE_C_FLAGS='-funsigned-char' \
- -DCMAKE_C_FLAGS_DEBUG='-g2 -O2' \
- -DCMAKE_CXX_FLAGS='-funsigned-char' \
- -DCMAKE_CXX_FLAGS_DEBUG='-g2 -O2' \
- -DAPPEND_CPPFLAGS='-DBOOST_MULTI_INDEX_ENABLE_SAFE_MODE' \
+ -DCMAKE_BUILD_TYPE=Release \
 "
