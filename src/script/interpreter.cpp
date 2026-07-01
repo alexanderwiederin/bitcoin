b2a517f30e5e43e5a9e12c6d7db6d365544c5879
sedited <seb.kung@gmail.com>
2026-07-01 13:45:04 +0200

    kernel: Add script tracer
    This adds a callback hook to the kernel library C header for getting the
    current script execution state on each script evaluation step. This can
    be used for debugging scripts and gives external applications a view
    into how Bitcoin Core does script evaluation.
    
    This feature is gated behind the ENABLE_SCRIPT_TRACE flag. To compile
    with the feature enabled, pass `-DENABLE_SCRIPT_TRACE=ON`. If the feature
    is not manually enabled, the trace hooks are stubbed out.
    
    The traces are divided into separate frames. RAII is used to guarantee
    that every script execution has at least its start and end state
    captured. Each instruction generates a unique frame as well.
    
    The ScriptError in the VerifyScript invocation run when calling
    script_pubkey_verify is added in order to be able to surface the error
    code in the trace frames.
    
    (cherry picked from commit 20fb03490d1f38dde67f058acd68e816595d9222)

--- src/script/interpreter.cpp
+++ src/script/interpreter.cpp
@@ -11,6 +11,7 @@
 #include <prevector.h>
 #include <pubkey.h>
 #include <script/script.h>
+#include <script/trace.h>
 #include <script/val64.h>
 #include <script/valtype_stack.h>
 #include <script/varops.h>
@@ -463,6 +464,8 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
     execdata.m_codeseparator_pos = 0xFFFFFFFFUL;
     execdata.m_codeseparator_pos_init = true;
 
+    SCRIPT_TRACE_SCOPE(stack, script, opcode_pos, altstack, nOpCount, sigversion, execdata.m_tapleaf_hash_init ? execdata.m_tapleaf_hash.data() : nullptr, execdata.m_codeseparator_pos, serror);
+
     try
     {
         for (; pc < pend; ++opcode_pos) {
@@ -471,8 +474,12 @@ bool EvalScript(std::vector<std::vector<unsigned char> >& stack, const CScript& 
             //
             // Read instruction
             //
-            if (!script.GetOp(pc, opcode, vchPushValue))
+            if (!script.GetOp(pc, opcode, vchPushValue)) {
                 return set_error(serror, SCRIPT_ERR_BAD_OPCODE);
+            }
+
+            SCRIPT_TRACE_STEP(fExec, opcode);
+
             if (vchPushValue.size() > MAX_SCRIPT_ELEMENT_SIZE)
                 return set_error(serror, SCRIPT_ERR_PUSH_SIZE);
 
